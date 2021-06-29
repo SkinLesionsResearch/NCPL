6cbf085dbd6b0383aa0ac0504f62e37d79309d5d
jackieQizhu <jackiecococa@gmail.com>
2021-06-30 00:39:32 +0800

    refactors the training scripts

--- run_training.sh
+++ run_training.sh
@@ -1,4 +1,8 @@
 current_date_time=$(date +%Y%m%d%H%M%S)
 echo "start_time: ""$current_date_time" >> run.log
 cp ./object/train.py .
-nohup python -u ./train.py --src-dset-path './data/semi_processed_two_cates' --labeled_num 2500 --num_classes 2 > run.log&
+
+nohup python -u ./train.py \
+--src-dset-path './data/semi_processed_two_cates' \
+--labeled_num 2500 \
+--num_classes 2 > run.log&
