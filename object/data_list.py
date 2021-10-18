1ddf237ed9563825f572f22113075271af087d6e
jackieQizhu <jackiecococa@gmail.com>
2021-10-18 20:06:59 +0800

    Still Dev

--- object/data_list.py
+++ object/data_list.py
@@ -91,6 +91,32 @@ class ImageList(Dataset):
     def __len__(self):
         return len(self.imgs)
 
+class ImageListWithPath(Dataset):
+    def __init__(self, image_list, args, labels=None, transform=None, target_transform=None, mode='RGB'):
+        imgs = make_dataset(image_list, labels, args)
+        if len(imgs) == 0:
+            raise (RuntimeError("Found 0 images in subfolders of: " + root + "\n" +
+                                 "Supported image extensions are: " + ",".join(IMG_EXTENSIONS)))
+        self.imgs = imgs
+        self.transform = transform
+        self.target_transform = target_transform
+        if mode == 'RGB':
+            self.loader = rgb_loader
+        elif mode == 'L':
+            self.loader = l_loader
+
+    def __getitem__(self, index):
+        path, target = self.imgs[index]
+        img = self.loader(path)
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return img, target, path
+
+    def __len__(self):
+        return len(self.imgs)
+
 class ImageList_idx(Dataset):
     def __init__(self, image_list, args, labels=None, transform=None, target_transform=None, mode='RGB'):
         imgs = make_dataset(image_list, labels, args)
--- object/gen_cam.py
+++ object/gen_cam.py
@@ -9,7 +9,7 @@ import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from PIL import ImageFile, Image
-from data_list import ImageList
+from data_list import ImageList, ImageListWithPath
 from torch.utils.data import DataLoader
 import utils
 import numpy as np
@@ -40,37 +40,35 @@ def get_test_data(loader, net):
             data = iter_test.next()
             inputs = data[0]
             labels = data[1]
+            path = data[2]
             inputs = inputs.cuda()
             features, outputs = net(inputs)
             if start_test:
                 all_features = features.float().cpu()
                 all_output = outputs.float().cpu()
                 all_label = labels.float()
+                all_path = path
                 start_test = False
             else:
                 all_features = torch.cat((all_features, features.float().cpu()), 0)
                 all_output = torch.cat((all_output, outputs.float().cpu()), 0)
                 all_label = torch.cat((all_label, labels.float()), 0)
+                all_path += path
     _, predict = torch.max(all_output, 1)
-    return all_features, all_output, all_label, predict
+    return all_features, all_output, all_label, predict, all_path
 
 
 def return_CAM(feature_conv, weight, class_idx):
     # generate the class -activation maps upsample to 256x256
     size_upsample = (256, 256)
     bz, nc, h, w = feature_conv.shape
-    print(feature_conv.shape)
-    print(weight.shape)
+
     output_cam = []
     for idx in class_idx:
         feature_conv = feature_conv.reshape((nc, h * w))
         cam = np.matmul(weight[idx], feature_conv)
         cam = cam.reshape(h, w)
-        print(cam.shape)
-        print(cam)
-        print(np.min(cam))
         cam = cam - np.min(cam)
-        print(cam, ", ", np.max(cam))
         cam_img = cam / np.max(cam)
         cam_img = np.uint8(255 * cam_img)
         output_cam.append(cv2.resize(cam_img, size_upsample))
@@ -96,17 +94,16 @@ def get_data_loaders(args, test_fname):
     dsets = {}
     dset_loaders = {}
     test_txt = open(os.path.join(args.dset_path, test_fname)).readlines()
-    dsets[test_fname] = ImageList(test_txt, args, transform=image_test())
+    dsets[test_fname] = ImageListWithPath(test_txt, args, transform=image_test())
     dset_loaders[test_fname] = DataLoader(dsets[test_fname], batch_size=args.batch_size, shuffle=True,
                                           num_workers=args.worker, drop_last=False)
 
     return dset_loaders
 
 
-def target_logic(args):
+def target_logic(args, suffix):
     args.batch_size = 32
-    test_loader = get_data_loaders(args, "test_cam.txt")["test_cam.txt"]
-    print(test_loader)
+    test_loader = get_data_loaders(args, "test_cam_1.txt")["test_cam_1.txt"]
 
     net = utils.get_model(args.net, args.num_classes)
     print(args.model_path)
@@ -115,46 +112,40 @@ def target_logic(args):
     print(is_model_state_dict_valid)
     net.eval()
 
-    features, logits, y_true, y_predict = get_test_data(test_loader, net)
-    h_x = F.softmax(logits, dim=1).data.squeeze()
+    _, logits, _, _, all_path = get_test_data(test_loader, net)
 
+    img_path = all_path[0]
+    logits = logits[0]
+    h_x = F.softmax(logits, dim=0).data.squeeze()
     probs, idx = torch.sort(h_x)
     probs = probs.detach().numpy()
     idx = idx.numpy()
-
-    predicted_labels = []
-    predicted_labels.append(idx[0])
 
     model_state_dict = net.state_dict()
     # for k, v in model_state_dict:
     #     print(k, ", ", v)
 
-    # Print model's state_dict
-    print("Model's state_dict:")
-    for param_tensor in model_state_dict:
-        print(param_tensor, "\t", model_state_dict[param_tensor].size())
-
     fc_weights = model_state_dict["classifier.weight"].cpu().numpy()  # [2,2048]
-    print(fc_weights.shape)
     features_conv = net.ori_features_ly4.cpu().numpy()
     CAMs = return_CAM(features_conv, fc_weights, [idx[0]])
-    print(np.array(CAMs).shape)
     flist_path = os.path.join(args.dset_path, "test_cam.txt")
     test_txt = open(flist_path).readlines()
 
-    img_path = "./../ups/data_ham10000/datasets/ISIC_0024684.jpg"
-    ori_img = cv2.imread(img_path)
-    height, width, _ = ori_img.shape
+    img_ori = cv2.imread(img_path)
+    height, width, _ = img_ori.shape
     heatmap = cv2.applyColorMap(cv2.resize(CAMs[0], (width, height)), cv2.COLORMAP_JET)
-    result = heatmap * 0.5 + ori_img * 0.5
-    cv2.imwrite("testing_cam.jpg", result)
+    img_cam = heatmap * 0.5 + img_ori * 0.5
+    img_name = img_path.split("/")[-1].split(".")[0]
+    cv2.imwrite(img_name + ".jpg", img_ori)
+    cv2.imwrite(img_name + "_" + suffix + "_cam.jpg", img_cam)
 
-    # ori_img_list = [(img_path, cv2.imread(img_path)) for img_path in test_txt.split(" ")[0]]
-    # for img_path, ori_img in ori_img_list:
-    #     height, width, _ = ori_img.shape
-    #     heatmap = cv2.applyColorMap(cv2.resize(CAMs[0], (width, height)), cv2.COLORMAP_JET)
-    #     result = heatmap * 0.5 + ori_img * 0.5
-    #     cv2.imwrite((img_path.split("/")[-1][-4] + "_cam"+".jpg"), result)
+
+def gen_path():
+    args.net = os.path.basename(args.subDir).split('_')[0]
+    args.output_dir_train = ""
+    args.output_dir_train = os.path.join(args.output_dir_train, args.dir)
+    args.output_dir_train = os.path.join(args.output_dir_train, args.subDir)
+    args.model_path = args.output_dir_train + '/best_params.pt'
 
 
 if __name__ == "__main__":
@@ -175,9 +166,19 @@ if __name__ == "__main__":
     parser.add_argument('--save_dir', type=str, default=None)
 
     args = parser.parse_args()
-    args.net = os.path.basename(args.subDir).split('_')[0]
-    args.output_dir_train = os.path.join(args.output_dir_train, args.dir)
-    args.output_dir_train = os.path.join(args.output_dir_train, args.subDir)
-    args.model_path = args.output_dir_train + '/best_params.pt'
 
-    target_logic(args)
+    args.dset_path = './data/semi_processed_bcc'
+
+    # gen imgs for the ncpl
+    args.dir = "ckps"
+    args.subDir = "resnet50_tc_bcc_2500_0.99_naive_0_afm_0.7_u_0.3"
+
+    gen_path()
+    target_logic(args, "ncpl")
+
+    # gen imgs for the baseline resnet50
+    args.dir = "ckps_bl"
+    args.subDir = "resnet50_tc_bcc_2500_0.0_naive_0_afm_0.7_u_0.3"
+
+    gen_path()
+    target_logic(args, "bl")
