2561e00d4429ad383b494b063a99e8f0bc33dadd
jackieQizhu <jackiecococa@gmail.com>
2021-10-25 00:03:00 +0800

    Dev

--- useful_scripts/chain.sh
+++ useful_scripts/chain.sh
@@ -9,49 +9,91 @@ function run_check() {
 		echo "run $1-experiment successfully, come to the next experiment"
 	fi 
 }
-
-:<<comment
-python -u /home/jackie/ResearchArea/SkinCancerResearch/semi_skin_cancer/train.py \
---src-dset-path './data/semi_processed' \
---labeled_num 500 \
---num_classes 7 \
---suffix 'sev_cates'
-
-run_check 500_two_cate
-sleep 5s
-
-python -u /home/jackie/ResearchArea/SkinCancerResearch/semi_skin_cancer/train.py \
+python -u /home/jackie/ResearchArea/SkinCancerResearch/semi_skin_cancer/object/train.py \
 --src-dset-path './data/semi_processed' \
 --labeled_num 1000 \
+--weight-afm 0.9 \
+--weight-u 0.1 \
 --num_classes 7 \
---suffix 'sev_cates'
+--suffix "acfi_1000"
 
-run_check 1000_two_cate
-sleep 5s
+run_check 1000_acfi_0901
 
-python -u /home/jackie/ResearchArea/SkinCancerResearch/semi_skin_cancer/train.py \
+python -u /home/jackie/ResearchArea/SkinCancerResearch/semi_skin_cancer/object/train.py \
 --src-dset-path './data/semi_processed' \
---labeled_num 1500 \
+--labeled_num 1000 \
+--weight-afm 0.7 \
+--weight-u 0.3 \
 --num_classes 7 \
---suffix 'sev_cates'
-
-run_check 1500_two_cate
-sleep 5s
-
-python -u /home/jackie/ResearchArea/SkinCancerResearch/semi_skin_cancer/train.py \
---src-dset-path './data/semi_processed' \
---labeled_num 2000 \
---num_classes 7 \
---suffix 'sev_cates'
-
-run_check 2000_two_cate
-sleep 5s
-comment
+--suffix "acfi_1000"
+run_check 1000_acfi_0703
 
 python -u /home/jackie/ResearchArea/SkinCancerResearch/semi_skin_cancer/object/train.py \
 --src-dset-path './data/semi_processed' \
 --labeled_num 2500 \
+--weight-afm 0.9 \
+--weight-u 0.1 \
 --num_classes 7 \
---suffix 'mc_base'
+--suffix "acfi_2500"
 
-run_check 2500_two_cate
+run_check 2500_acfi_0901
+
+python -u /home/jackie/ResearchArea/SkinCancerResearch/semi_skin_cancer/object/train.py \
+--src-dset-path './data/semi_processed' \
+--labeled_num 2500 \
+--weight-afm 0.7 \
+--weight-u 0.3 \
+--num_classes 7 \
+--suffix "acfi_2500"
+
+run_check 2500_acfi_0703
+
+python -u /home/jackie/ResearchArea/SkinCancerResearch/semi_skin_cancer/object/train.py \
+--src-dset-path './data/semi_processed' \
+--labeled_num 2500 \
+--weight-afm 0.6 \
+--weight-u 0.4 \
+--num_classes 7 \
+--suffix "acfi_2500"
+
+run_check 2500_acfi_0604
+
+python -u /home/jackie/ResearchArea/SkinCancerResearch/semi_skin_cancer/object/train.py \
+--src-dset-path './data/semi_processed' \
+--labeled_num 2500 \
+--weight-afm 0.5 \
+--weight-u 0.5 \
+--num_classes 7 \
+--suffix "acfi_2500"
+
+run_check 2500_acfi_0505
+
+python -u /home/jackie/ResearchArea/SkinCancerResearch/semi_skin_cancer/object/train.py \
+--src-dset-path './data/semi_processed' \
+--labeled_num 2500 \
+--weight-afm 0.4 \
+--weight-u 0.6 \
+--num_classes 7 \
+--suffix "acfi_2500"
+
+run_check 2500_acfi_0406
+
+python -u /home/jackie/ResearchArea/SkinCancerResearch/semi_skin_cancer/object/train.py \
+--src-dset-path './data/semi_processed' \
+--labeled_num 2500 \
+--weight-afm 0.3 \
+--weight-u 0.7 \
+--num_classes 7 \
+--suffix "acfi_2500"
+
+run_check 2500_acfi_0307
+
+python -u /home/jackie/ResearchArea/SkinCancerResearch/semi_skin_cancer/object/train.py \
+--src-dset-path './data/semi_processed' \
+--labeled_num 2500 \
+--weight-afm 0.1 \
+--weight-u 0.9 \
+--num_classes 7 \
+--suffix "acfi_2500"
+
+run_check 2500_acfi_0109
