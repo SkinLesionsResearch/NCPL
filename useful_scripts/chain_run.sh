c1cee6ed8bc5c586412a713278befc9c41813912
jackieQizhu <jackiecococa@gmail.com>
2021-10-26 12:40:58 +0800

    Dev

--- useful_scripts/chain_run.sh
+++
@@ -1,9 +0,0 @@
-if [ $# -eq 0 ]
-	then echo "No arguments supplied, please chain script to run the chain jobs"
-	exit
-fi
-current_date_time=$(date +%Y%m%d%H%M%S)
-touch chain.log
-echo "start_time: ""$current_date_time" >> chain.log
-cp ./object/train.py .
-nohup ./$1 > chain.log &
