72e16434181a5ed762878651f4f583534a1365ba
jackieQizhu <jackiecococa@gmail.com>
2021-11-02 04:14:07 +0800

    Dev

--- useful_scripts/rm_sth.sh
+++
@@ -1,9 +0,0 @@
-rm_exist() {
-    [ -e $1 ] && rm $1
-}
-rm -rf ckps
-rm -rf results
-rm -rf checkpoints
-rm_exist tb.log
-rm_exist run.log
-rm_exist chain.log
--- useful_scripts/run_tb.sh
+++
@@ -1,5 +0,0 @@
-if [ $# -eq 0 ]
-	then echo "No arguments supplied, please input path to tb"
-	exit
-fi
-nohup tensorboard --logdir=$1 --port 22389 > tb.log&
